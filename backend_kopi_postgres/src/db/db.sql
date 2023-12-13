6c840598b1a6eb3208268f3e285e8f4e53f499da
Glen <glen.tanyonghao@gmail.com>
2023-12-13 16:28:57 +0800

    Updating Table Creation in Postgres
    Updated constraint table for user_role.
    
    Created tables and its relevant constraint tables for beans and equipment table.
    
    Have yet to settle the surrogate tables for beans.equipment, user.beans and user.equipment.

--- backend_kopi_postgres/src/db/db.sql
+++ backend_kopi_postgres/src/db/db.sql
@@ -2,22 +2,64 @@ CREATE DATABASE kopi;
 
 CREATE EXTENSION if not exists "uuid-ossp";
 
--- Create users Table, set extension
+-- Create "users" table, set extension.
 CREATE TABLE users (
     user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_name VARCHAR(200) NOT NULL,
     user_email VARCHAR(200) NOT NULL,
     user_password VARCHAR(200) NOT NULL,
-    user_role VARCHAR(200) NOT NULL
+    user_role VARCHAR(200) NOT NULL,
+    FOREIGN KEY (user_role) REFERENCES roles(role)
 );
 
--- Create beans Table
+-- Create "roles" table.
+CREATE TABLE roles (
+    role VARCHAR(200) PRIMARY KEY DEFAULT 'user' 
+);
+
+-- Insert values into "roles" table.
+INSERT INTO roles (role) VALUES ('admin');
+INSERT INTO roles (role) VALUES ('user');
+
+-- Create "beans" table
 CREATE TABLE beans (
-    beans_id 
-    beans_country
-    beans_region
-    beans_type
-    beans_taste
-beans_roastdate
-beans
-)
+    beans_id SERIAL PRIMARY KEY,
+    beans_country VARCHAR(100) NOT NULL,
+    -- FOREIGN KEY (beans_country) REFERENCES countries(code)
+    beans_region VARCHAR(200) NOT NULL,
+    beans_type VARCHAR(200) NOT NULL,
+    FOREIGN KEY (beans_type) REFERENCES beansType(beans_type),
+    beans_taste VARCHAR(200) NOT NULL,
+    beans_roastdate DATE NOT NULL,
+    beans_prevgrindsize DECIMAL NOT NULL,
+    beans_remarks VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE beansType (
+    beans_type VARCHAR(200) PRIMARY KEY
+);
+
+
+INSERT INTO beansType (beans_type) VALUES ('espresso');
+INSERT INTO beansType (beans_type) VALUES ('filter');
+
+-- CREATE TABLE countries (
+--     code VARCHAR(2) PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL UNIQUE
+-- )
+
+CREATE TABLE equipment (
+    equipment_id SERIAL PRIMARY KEY,
+    equipment_type VARCHAR(200) NOT NULL,
+    FOREIGN KEY (equipment_type) REFERENCES equipmentType(equipment_type),
+    equipment_model VARCHAR(200) NOT NULL,
+    equipment_modification VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE equipmentType (
+    equipment_type VARCHAR(200) PRIMARY KEY
+);
+
+INSERT INTO equipmentType (equipment_type) VALUES ('grinder');
+INSERT INTO equipmentType (equipment_type) VALUES ('espresso machine');
+INSERT INTO equipmentType (equipment_type) VALUES ('v60');
